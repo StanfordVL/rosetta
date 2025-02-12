3e69523fdce47c3bae297ef58d228c47676a2919
sanjanasrivastava <sanjana2@stanford.edu>
2025-02-12 12:48:56 -0800

    requirements, bit of cleanup in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='rosetta',
     version='0.1.0',
+    author="Sanjana Srivastava, Kangrui Wang, Yung-Chieh Chan",
+    author_email="sanjana2@stanford.edu",
     packages=find_packages(),
-    install_requires=[],  # Add dependencies here, if any
+    install_requires=[
+        "astor",
+        "ipython",
+        "openai",
+        "opencv_python",
+        "Pillow",
+        "redbaron",
+        "setuptools"
+    ]
 )
